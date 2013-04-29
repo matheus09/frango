49ae916a353f113a8e8838ac39e39e3e3154f240
Augusto Severo <guteco@gmail.com>
2013-04-29 14:01:57 -0300

    Atualização SQL

--- tecnotec_frango.sql
+++ tecnotec_frango.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Máquina: localhost
--- Data de Criação: 24-Mar-2013 às 12:50
+-- Data de Criação: 29-Abr-2013 às 13:59
 -- Versão do servidor: 5.0.91-community-cll
--- versão do PHP: 5.2.9
+-- versão do PHP: 5.3.17
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -32,7 +32,14 @@ CREATE TABLE IF NOT EXISTS `administrador` (
   `login` varchar(20) NOT NULL,
   `senha` varchar(20) NOT NULL,
   PRIMARY KEY  (`codAdmin`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Extraindo dados da tabela `administrador`
+--
+
+INSERT INTO `administrador` (`codAdmin`, `nome`, `login`, `senha`) VALUES
+(1, 'Administrador', 'admin', 'admin');
 
 -- --------------------------------------------------------
 
@@ -43,12 +50,21 @@ CREATE TABLE IF NOT EXISTS `administrador` (
 CREATE TABLE IF NOT EXISTS `cliente` (
   `idCliente` int(2) NOT NULL auto_increment,
   `nome` varchar(20) NOT NULL,
-  `cpf` int(11) NOT NULL,
-  `telefone` varchar(8) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `telefone` varchar(14) NOT NULL,
   `endereco` varchar(50) NOT NULL,
   `dataNascimento` varchar(10) NOT NULL,
   PRIMARY KEY  (`idCliente`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=27 ;
+
+--
+-- Extraindo dados da tabela `cliente`
+--
+
+INSERT INTO `cliente` (`idCliente`, `nome`, `cpf`, `telefone`, `endereco`, `dataNascimento`) VALUES
+(23, 'yago', '100.738.074-86', '(84) 5565-5551', 'Rua João de Deus', '1992-06-08'),
+(26, 'Diego Olinto', '053.173.214-25', '(84) 3643-1566', 'Rua Sao Joao', '1991-08-21'),
+(25, 'Junior', '091.103.704-71', '(84) 9996-0115', 'Rua das perfideas', '1992-03-07');
 
 -- --------------------------------------------------------
 
@@ -75,7 +91,14 @@ CREATE TABLE IF NOT EXISTS `fornecedor` (
   `cnpj` int(11) NOT NULL,
   `telefone` varchar(8) NOT NULL,
   PRIMARY KEY  (`idFornecedor`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Extraindo dados da tabela `fornecedor`
+--
+
+INSERT INTO `fornecedor` (`idFornecedor`, `nome`, `endereco`, `cnpj`, `telefone`) VALUES
+(1, 'lol', 'okokok', 0, '');
 
 -- --------------------------------------------------------
 
@@ -86,15 +109,23 @@ CREATE TABLE IF NOT EXISTS `fornecedor` (
 CREATE TABLE IF NOT EXISTS `funcionario` (
   `idFuncionario` int(2) NOT NULL auto_increment,
   `nome` varchar(20) NOT NULL,
-  `cpf` int(11) NOT NULL,
-  `telefone` varchar(8) NOT NULL,
+  `cpf` varchar(14) NOT NULL,
+  `telefone` varchar(14) NOT NULL,
   `dataNascimento` varchar(10) NOT NULL,
   `salario` float NOT NULL,
   `endereco` varchar(50) NOT NULL,
   `login` varchar(20) NOT NULL,
   `senha` varchar(20) NOT NULL,
   PRIMARY KEY  (`idFuncionario`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+
+--
+-- Extraindo dados da tabela `funcionario`
+--
+
+INSERT INTO `funcionario` (`idFuncionario`, `nome`, `cpf`, `telefone`, `dataNascimento`, `salario`, `endereco`, `login`, `senha`) VALUES
+(9, 'Maria Prea', '035.364.784-50', '(34) 1744-4455', '1977-11-24', 1500, 'Rua dos prea 100', 'totonha', 'maria'),
+(13, 'Garrafa Quebrada', '091.103.704-71', '(84) 9819-8347', '1992-03-07', 45, 'Rua Sao Joao', 'garrafa', 'cerveja');
 
 -- --------------------------------------------------------
 
